9b9a7dc8da7c236892c609cb9039dbcceba8437c
Matej Klima <matej.klima5@gmail.com>
2024-05-04 18:20:17 +0200

    Create example matches

--- DB/Utils/DatabaseSeeder.cs
+++ DB/Utils/DatabaseSeeder.cs
@@ -1,5 +1,6 @@
 using DB.Models;
 using DB.Models.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace DB.Utils;
 
@@ -14,6 +15,104 @@ public static class DatabaseSeeder
         SeedData(context);
     }
     public static void SeedData(DatabaseContext context)
+    {
+        var league = new League { Name = "Czech Extraliga", Country = "Czech Republic" };
+        context.Leagues.Add(league);
+        
+        var season22 = new Season { Year = 2022, League = league };
+        var season23 = new Season { Year = 2023, League = league };
+        context.Seasons.Add(season22);
+        context.Seasons.Add(season23);
+        
+        
+        SeedTeams(context);
+        SeedMatches(context, season22);
+        SeedMatches(context, season23);
+    }
+
+    private static void SeedMatches(DatabaseContext context, Season season)
+    {
+        var teams = context.Teams.Include(team => team.Players).ToList();
+        var random = new Random();
+        
+        for (int i = 0; i < teams.Count; i++)
+        {
+            for (int j = i + 1; j < teams.Count; j++)
+            {
+                Team homeTeam = teams[i];
+                Team awayTeam = teams[j];
+                
+                int homeGoals = random.Next(0, 5);
+                int awayGoals = random.Next(0, 5);
+
+                MatchResult result;
+                if (homeGoals > awayGoals)
+                {
+                    result = MatchResult.HomeWin;
+                }
+                else if (homeGoals < awayGoals)
+                {
+                    result = MatchResult.AwayWin;
+                }
+                else
+                {
+                    homeGoals++;
+                    result = MatchResult.HomeOTWin;
+                }
+                
+                var match = new Match
+                {
+                    HomeTeam = homeTeam,
+                    AwayTeam = awayTeam,
+                    Date = DateTime.Now,
+                    Time = TimeSpan.FromHours(random.Next(0, 24)),
+                    Location = "Random location",
+                    Season = season,
+                    MatchResult = result
+                };
+
+                var homePlayers = homeTeam.Players.ToList();
+                for (int k = 0; k < homeGoals; k++)
+                {
+                    var scorer = homePlayers[random.Next(0, homePlayers.Count)];
+                    var time = TimeSpan.FromSeconds(random.Next(0, 3600));
+                    match.Goals.Add(new Goal { TimeScored = time, Scorer = scorer, IsHomeTeamGoal = true, Match = match });
+                }
+
+                var awayPlayers = awayTeam.Players.ToList();
+                for (int k = 0; k < awayGoals; k++)
+                {
+                    var scorer = awayPlayers[random.Next(0, awayPlayers.Count)];
+                    var time = TimeSpan.FromSeconds(random.Next(0, 3600));
+                    match.Goals.Add(new Goal { TimeScored = time, Scorer = scorer, IsHomeTeamGoal = false, Match = match });
+                }
+
+                int homePenalties = random.Next(0, 10);
+                for (int k = 0; k < homePenalties; k++)
+                {
+                    var penaltyType = (PenaltyType) random.Next(0, Enum.GetValues<PenaltyType>().Length);
+                    var player = homePlayers[random.Next(0, homePlayers.Count)];
+                    var time = TimeSpan.FromSeconds(random.Next(0, 3600));
+                    match.Penalties.Add(new Penalty { PenaltyType = penaltyType, Player = player, IsHomeTeamPenalty = true, TimeTaken = time, Match = match });
+                }
+
+                int awayPenalties = random.Next(0, 10);
+                for (int k = 0; k < awayPenalties; k++)
+                {
+                    var penaltyType = (PenaltyType) random.Next(0, Enum.GetValues<PenaltyType>().Length);
+                    var player = awayPlayers[random.Next(0, awayPlayers.Count)];
+                    var time = TimeSpan.FromSeconds(random.Next(0, 3600));
+                    match.Penalties.Add(new Penalty { PenaltyType = penaltyType, Player = player, IsHomeTeamPenalty = false, TimeTaken = time, Match = match });
+                }
+
+                context.Matches.Add(match);
+            }
+        }
+
+        context.SaveChanges();
+    }
+    
+    private static void SeedTeams(DatabaseContext context)
     {
         if (context.Teams.Any()) return;
 
