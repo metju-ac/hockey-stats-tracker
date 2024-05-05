f8ec6567bfd8abf5a0b33743dd367707805f73d9
Matej Klima <matej.klima5@gmail.com>
2024-05-06 01:18:58 +0200

    Display standings

---
+++ HockeyStatsTracker/Models/TeamStandingFE.cs
@@ -0,0 +1,18 @@
+namespace HockeyStatsTracker.Models;
+
+public class TeamStandingFE
+{
+    public int TeamId { get; set; }
+    public string TeamName { get; set; }
+    public int GamesPlayed { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int OTWins { get; set; }
+    public int OTLosses { get; set; }
+    public int ShootoutWins { get; set; }
+    public int ShootoutLosses { get; set; }
+    public int GoalsFor { get; set; }
+    public int GoalsAgainst { get; set; }
+    public int Points { get; set; }
+    public int Rank { get; set; }
+}
