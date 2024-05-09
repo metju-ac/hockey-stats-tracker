3c7419f29a0c22146347db4390082a81e47e3976
Matej Klima <matej.klima5@gmail.com>
2024-05-09 17:01:08 +0200

    feat: add quickgrids

--- HockeyStatsTracker/Program.cs
+++ HockeyStatsTracker/Program.cs
@@ -13,17 +13,13 @@ builder.Services.AddDbContext<DatabaseContext>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
 
-
-// Add the MatchesController
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
